7be133ea6de9347fe096286e39812f6b381ae962
unknown <luisjavierescobar982u@gmail.com>
2025-05-14 22:42:21 -0600

    agrego modulo semana 4.py

---
+++ modulo-semana4.py
@@ -0,0 +1,11 @@
+# numhuevos = 12
+# # opción 1
+# print('tengo' + str(numhuevos)+ 'huevos.')
+# # opción 2
+# print ('tengo %s huevos' % (numhuevos ))
+
+# Calcular la superficie o área de un cuadrado 
+# lado = int (input ("Ingrese la medida del cuadrado: "))
+# superficie = lado * lado 
+# print ("la superficie del cuadrado es: " + str(superficie))
+
