0b846a9ed9ae59bd1a502ea8bf9e0241af5c3410
unknown <luisjavierescobar982u@gmail.com>
2025-05-29 22:54:01 -0600

    while1semana6.py

---
+++ while1-semana6.py
@@ -0,0 +1,13 @@
+print ("Impares menores a 10")
+x = 1
+while x <= 10:
+    print (x)
+    x  += 2 
+
+factorial = 5 
+contador = factorial - 1
+while contador > 0 :
+    factorial *= contador
+    contador -= 1
+
+print ("el factorial de 5 es", factorial)
